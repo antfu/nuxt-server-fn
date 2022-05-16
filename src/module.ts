6b97facd1496debcca7a59d5c0fb94475b117ceb
Anthony Fu <anthonyfu117@hotmail.com>
2022-05-16 14:21:39 +0800

    fix: add transpile target

--- src/module.ts
+++ src/module.ts
@@ -1,10 +1,13 @@
 import { dirname, join, resolve } from 'path'
+import { fileURLToPath } from 'url'
 import { defineNuxtModule } from '@nuxt/kit'
 import fs from 'fs-extra'
 import { ROUTE } from './runtime/constant'
 
 export interface ModuleOptions {
 }
+
+const dir = dirname(fileURLToPath(import.meta.url))
 
 export default defineNuxtModule<ModuleOptions>({
   meta: {
@@ -16,6 +19,19 @@ export default defineNuxtModule<ModuleOptions>({
   async setup(options, nuxt) {
     const root = nuxt.options.rootDir
     const apiPath = resolve(root, `server/api/${ROUTE}.ts`)
+
+    nuxt.hook('config', (options) => {
+      options.build.transpile.push(join(dir, 'runtime/client'))
+    })
+
+    nuxt.hook('autoImports:extend', (imports) => {
+      imports.push({
+        from: clientPath,
+        name: 'useServerFn',
+        as: 'useServerFn',
+      })
+    })
+
     await fs.ensureDir(dirname(apiPath))
     await fs.writeFile(apiPath, `
 // generate by nuxt-server-fn
@@ -32,13 +48,5 @@ import type * as functions from '~/server/fn'
 
 export const useServerFn = createServerFnClient<typeof functions>()
 `.trimStart())
-
-    nuxt.hook('autoImports:extend', (imports) => {
-      imports.push({
-        from: clientPath,
-        name: 'useServerFn',
-        as: 'useServerFn',
-      })
-    })
   },
 })
