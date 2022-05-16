8267bf02dc9b8fdea79b8296470d0b24e3b0aba3
Anthony Fu <anthonyfu117@hotmail.com>
2022-05-16 14:34:40 +0800

    fix: transpile client

--- src/module.ts
+++ src/module.ts
@@ -1,13 +1,10 @@
 import { dirname, join, resolve } from 'path'
-import { fileURLToPath } from 'url'
 import { defineNuxtModule } from '@nuxt/kit'
 import fs from 'fs-extra'
 import { ROUTE } from './runtime/constant'
 
 export interface ModuleOptions {
 }
-
-const dir = dirname(fileURLToPath(import.meta.url))
 
 export default defineNuxtModule<ModuleOptions>({
   meta: {
@@ -19,9 +16,10 @@ export default defineNuxtModule<ModuleOptions>({
   async setup(options, nuxt) {
     const root = nuxt.options.rootDir
     const apiPath = resolve(root, `server/api/${ROUTE}.ts`)
+    const clientPath = join(nuxt.options.buildDir, 'server-fn-client.ts')
 
     nuxt.hook('config', (options) => {
-      options.build.transpile.push(join(dir, 'runtime/client'))
+      options.build.transpile.push('nuxt-server-fn/client')
     })
 
     nuxt.hook('autoImports:extend', (imports) => {
@@ -41,7 +39,6 @@ import * as functions from '../fn'
 export default createServerFnAPI(functions)
 `.trimStart())
 
-    const clientPath = join(nuxt.options.buildDir, 'server-fn-client.ts')
     await fs.writeFile(clientPath, `
 import { createServerFnClient } from 'nuxt-server-fn/client'
 import type * as functions from '~/server/fn'
